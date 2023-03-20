e4795248e90a6eac667d8fb68a3b7ff9a30846cc
unknown <deliah07@naver.com>
2023-03-20 15:56:56 +0900

    상세보기 이체내역 xml->DTO->Repository 작성

--- src/main/java/shop/mtcoding/bankapp/model/history/HistoryRepository.java
+++ src/main/java/shop/mtcoding/bankapp/model/history/HistoryRepository.java
@@ -3,6 +3,9 @@ package shop.mtcoding.bankapp.model.history;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import shop.mtcoding.bankapp.dto.history.HistoryRespDto;
 
 @Mapper
 public interface HistoryRepository {
@@ -13,6 +16,8 @@ public interface HistoryRepository {
     public int deleteById(int id);
 
     public List<History> findAll();
+
+    public List<HistoryRespDto> findByGubun(@Param("gubun") String gubun, @Param("accountId") int accountId);
 
     public History findById(int id);
 }
